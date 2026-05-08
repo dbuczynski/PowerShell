48f6fd6a9d93727662e3a65c923a5ed68a85ad49
Daniel Buczynski <Daniel.Buczynski@gmail.com>
2026-05-08 23:05:04 +0200

    chore: remove temp syntax check file

--- _chk.ps1
+++
@@ -1,6 +0,0 @@
-$errors = $null
-[System.Management.Automation.Language.Parser]::ParseFile(
-    'c:\Users\dbuczynski\Antigravity\GIT-dbuczynski\PowerShell\Publish-BUTCH.ps1',
-    [ref]$null, [ref]$errors)
-if ($errors.Count -eq 0) { Write-Host 'No syntax errors found.' -ForegroundColor Green }
-else { $errors | ForEach-Object { Write-Host "Line $($_.Extent.StartLineNumber) col $($_.Extent.StartColumnNumber): $($_.Message)" -ForegroundColor Red } }
